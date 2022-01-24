d116926859e865080efaa9c1432ece20994cb159
+ <+>
2022-01-24 11:25:01 +0000

    commit tree dbac20f826903cff5a259ebb3ad9a651cb50c821

--- app.rb
+++ app.rb
@@ -4,6 +4,32 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+get '/' do 
 
-  end
+  erb :root
+end
+
+get '/new' do 
+
+  erb :'pirates/new'
+end
+
+
+post '/pirates' do 
+
+  # @pirate = Pirate.new(params[:pirate])
+
+  @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+
+  params[:pirate][:ships].each do |details|
+    Ship.new(details[:name], details[:type], details[:booty])
+  end 
+
+  @ships = Ship.all
+
+  erb :'pirates/show'
+
+end 
+end
+  
 end
